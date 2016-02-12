188f8ec516643fc4d843e81771a9fe6336cbf190
Morten la Cour Dragheim <Morten@Mortens-MacBook-Pro.local>
2016-02-13 00:49:26 +0100

    first rev of project

--- Kalender/ViewController.swift
+++ Kalender/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var beskedLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterByTextField: UITextField!
+    @IBOutlet weak var clickButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func sendBeskedButtonPress(sender: UIButton) {
+        beskedLabel.hidden = false
+        beskedLabel.text = enterByTextField.text
+        beskedLabel.textColor = UIColor.redColor()
+
+        
+        enterByTextField.text = ""
+        enterByTextField.resignFirstResponder()
+        clickButton.setTitle("Mail sent", forState: UIControlState.Normal)
+        clickButton.setTitleColor(UIColor.purpleColor(), forState: UIControlState.Normal)
+    }
 
 }
 
